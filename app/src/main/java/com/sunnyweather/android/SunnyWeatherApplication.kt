0fb8f956faf5f1964cbc349df655451e71bfe695
wanting-y <wanting.y@outlook.com>
2020-12-31 17:37:22 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+import android.media.session.MediaSession
+
+class SunnyWeatherApplication : Application() {
+    companion object{
+        const val TOKEN = "24lhlMKeTzKuerTY"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
