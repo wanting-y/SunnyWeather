17986ce59c9b03bb51e6828c292bbbaaeb2442a5
wanting-y <wanting.y@outlook.com>
2021-01-03 13:58:04 +0800

    修改App的图标

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Place
@@ -25,22 +24,23 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-//            val activity = fragment.activity
-//            if (activity is WeatherActivity) {
-//                activity.drawerLayout.closeDrawers()
-//                activity.viewModel.locationLng = place.location.lng
-//                activity.viewModel.locationLat = place.location.lat
-//                activity.viewModel.placeName = place.name
-//                activity.refreshWeather()
-//            } else {
+            val activity = fragment.activity
+            if (activity is WeatherActivity) {
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            } else {
                 val intent = Intent(parent.context, WeatherActivity::class.java).apply {
                     putExtra("location_lng", place.location.lng)
                     putExtra("location_lat", place.location.lat)
                     putExtra("place_name", place.name)
                 }
-                fragment.viewModel.savePlace(place)
                 fragment.startActivity(intent)
-                fragment.activity?.finish()
+                activity?.finish()
+            }
+            fragment.viewModel.savePlace(place)
         }
         return holder
     }
@@ -50,6 +50,7 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         holder.placeName.text = place.name
         holder.placeAddress.text = place.address
     }
+
     override fun getItemCount() = placeList.size
 
 }
